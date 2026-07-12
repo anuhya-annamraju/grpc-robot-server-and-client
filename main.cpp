3800edfc49597b649b6df7184c63cde2e4fc202d
AJA <anuhyaja@gmail.com>
2026-07-12 12:38:20 +0200

    configure grpc plugin

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 
 int main() {
     printf("Hello, World!\n");
-    robot_status status;
+    RobotStatus status;
 
-    status.set_robotserialid(12345);
+    status.set_robotserialid(1234);
     status.set_latitude(37.7749);
     status.set_ismoving(true);
+
+    std::string serialized;
+
+   if( !status.SerializeToString(&serialized) ) {
+        std::cerr << "Failed to serialize status." << std::endl;
+        return 1;
+    }
 
     std::cout << "robot id is " << status.robotserialid() << std::endl;
     return 0;
